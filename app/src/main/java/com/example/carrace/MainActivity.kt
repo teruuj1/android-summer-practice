189e8deead6fb38d3a30bbad2f56002b99f0746e
teruuj1 <shuvalova05sofya@mail.ru>
2024-07-01 12:40:32 +0300

    commit

--- app/src/main/java/com/example/carrace/MainActivity.kt
+++ app/src/main/java/com/example/carrace/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.carrace
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.carrace.ui.theme.CarRaceTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CarRaceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        val editTextNumber = findViewById<EditText>(R.id.editTextNumber)
+        val raceButton = findViewById<Button>(R.id.raceButton)
+        var resultsTextView = findViewById<TextView>(R.id.resultTextView)
+
+        raceButton.setOnClickListener {
+            val numberOfCars = editTextNumber.text.toString().toIntOrNull()
+            if (numberOfCars != null) {
+                val automobiles = List(numberOfCars) { createRandomAutomobile() }
+                organizeRaces(automobiles, resultsTextView)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CarRaceTheme {
-        Greeting("Android")
     }
 }
